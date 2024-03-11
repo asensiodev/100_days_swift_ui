d2588cb6f094825476516b1af1199603c36e20fc
asensiodev <asensiodev@gmail.com>
2024-03-11 17:25:56 +0100

    Chapter 2

---
+++ chapter_02/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let celsiusTemperature = 34.0
+let fahrenheitTemperature: Float = ((34 * 9) / 5) + 5
+
+print("Convert Celsius to Fahrenheit")
+print("Celsius: \(celsiusTemperature)°C")
+print("Fahrenhet: \(fahrenheitTemperature)°F")
+
+
