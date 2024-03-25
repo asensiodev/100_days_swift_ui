edfa20f4a8c5fa33dae0873196dd560d47e99327
asensiodev <asensiodev@gmail.com>
2024-03-25 11:08:31 +0100

    Add variant

--- chapters_05_06/MyPlayground.playground/Contents.swift
+++ chapters_05_06/MyPlayground.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// FizzBuzz
+
+
+//for i in 1...100 {
+//    if ((i % 3 == 0) && (i % 5 == 0)) {
+//        print("FizzBuzz")
+//    } else if (i % 3 == 0) {
+//        print("Fizz")
+//    } else if (i % 5 == 0) {
+//        print("Buzz")
+//    } else {
+//        print("NONE")
+//    }
+//}
+
+
+// Functional programming variant
+for i in 1...100 {
+    if ((i.isMultiple(of: 3)) && (i.isMultiple(of: 5))) {
+        print("FizzBuzz")
+    } else if (i.isMultiple(of: 3)) {
+        print("Fizz")
+    } else if (i.isMultiple(of: 5)) {
+        print("Buzz")
+    } else {
+        print("NONE")
+    }
+}
